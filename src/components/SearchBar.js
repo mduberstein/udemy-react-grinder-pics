b551e8dfadc7739fb40980ac5790105190affe5d
Michael Duberstein <dubermic@flutterint.com>
2022-07-12 16:03:21 -0400

    Pics: in Clip 87 Solving Context Issues, after solution 3 to the Context problem

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-
-  state = {term: ''}
+  state = { term: "" };
   // arrow function binds this to the instance of the Search bar class
-  onFormSubmit = (event)=> {
-       event.preventDefault();
-       console.log(this.state.term);
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
-              value = {this.state.term}
-              onChange={e => this.setState({term: e.target.value})}
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
