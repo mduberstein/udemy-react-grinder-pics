bc2fefefa6dd05bea7b370768b9cdf793ee17867
Michael Duberstein <dubermic@flutterint.com>
2022-07-13 15:46:37 -0400

    Pics: After Clip 93 Viewing Request Results

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios'
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+      axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID XRR6Ew3YhY9B9bRtdSrfuktIS1-3AU9rNknnt7l8_0I'
+        }
+    } );
   }
 
   render() {
