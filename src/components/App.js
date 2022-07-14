a822dd49de6b75d8d4de9fc35c20ee3607a1f81d
Michael Duberstein <dubermic@flutterint.com>
2022-07-13 21:53:02 -0400

    Pics: After Clip 95 Setting State After Async Requests

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = {images: []};
   async onSearchSubmit(term) {
       const response = await axios.get('https://api.unsplash.com/search/photos', {
         params: { query: term },
@@ -10,13 +11,16 @@ class App extends React.Component {
           Authorization: 'Client-ID XRR6Ew3YhY9B9bRtdSrfuktIS1-3AU9rNknnt7l8_0I'
         }
       });
-      console.log(response.data.results);
+      console.log(this);
+      this.setState({images: response.data.results});
+      
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit} guessWhatIAam="I am the props pbject" />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
