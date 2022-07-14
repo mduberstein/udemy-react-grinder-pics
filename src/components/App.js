1ad300bf101d064548f0259652f0cbb6a8e21f34
Michael Duberstein <dubermic@flutterint.com>
2022-07-13 22:07:41 -0400

    Pics: After Clip 96 Binding Callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,20 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
   state = {images: []};
-  async onSearchSubmit(term) {
+  onSearchSubmit = async(term) => {
       const response = await axios.get('https://api.unsplash.com/search/photos', {
         params: { query: term },
         headers: {
           Authorization: 'Client-ID XRR6Ew3YhY9B9bRtdSrfuktIS1-3AU9rNknnt7l8_0I'
         }
       });
-      console.log(this);
-      this.setState({images: response.data.results});
-      
+      this.setState({images: response.data.results});  
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} guessWhatIAam="I am the props pbject" />
+        <SearchBar onSubmit={this.onSearchSubmit} />
         Found: {this.state.images.length} images.
       </div>
     );
