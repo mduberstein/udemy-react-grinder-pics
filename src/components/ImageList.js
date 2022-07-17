6bb579997c43837b1cffa0fb4e3112d8b1fdfd59
Michael Duberstein <dubermic@flutterint.com>
2022-07-17 13:22:33 -0400

    Pics: In Clip 102 Implementing Keys in Lists, before use of desctructuring

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const ImageList = props => {
-  console.log(props.images);
+  // console.log(props.images);
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} alt={image.alt_description} />
+    // Rule: add key only to the root element of the list: div in this case
+    // ALT 1 key is added to the div if it is the root element
+    // return <div key={image.id}>
+    //   <img src={image.urls.regular} alt={image.alt_description} />
+    // </div>
+    // ALT 2 key is added to the img element itself since it is the root element
+    return <img key={image.id} src={image.urls.regular} alt={image.description} />
   });
-  return <div>{images}</div>
+  return <div>{images}</div> 
 };
 
 export default ImageList;
