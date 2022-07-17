0100a3a77e5b2b9f790afad289011bcbdd781541
Michael Duberstein <dubermic@flutterint.com>
2022-07-17 13:28:39 -0400

    Pics: After Clip 102 Implementing Keys in Lists

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,14 +2,8 @@ import React from "react";
 
 const ImageList = props => {
   // console.log(props.images);
-  const images = props.images.map(image => {
-    // Rule: add key only to the root element of the list: div in this case
-    // ALT 1 key is added to the div if it is the root element
-    // return <div key={image.id}>
-    //   <img src={image.urls.regular} alt={image.alt_description} />
-    // </div>
-    // ALT 2 key is added to the img element itself since it is the root element
-    return <img key={image.id} src={image.urls.regular} alt={image.description} />
+  const images = props.images.map(({description, id, urls}) => {
+    return <img key={id} src={urls.regular} alt={description} />
   });
   return <div>{images}</div> 
 };
