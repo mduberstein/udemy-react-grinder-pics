7356a1cd6d6e1a5005cf122518b6d086ca0dab60
Michael Duberstein <dubermic@flutterint.com>
2022-07-18 04:16:56 -0400

    Pics: In Clip 111 Dynamic Spans, before last tweek to set row height to 10

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,24 +3,29 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {spans: 0};
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
     // console.log(this.imageRef);
     // console.log(this.imageRef.current.clientHeight);
-    // add vanilla JavaScript event listener
+    // add vanilla JavaScript event listener, to determine the height of the image only after it has really loaded
     this.imageRef.current.addEventListener('load', this.setSpans);
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    // console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 150);
+    this.setState({spans}); // ES2015 shortcut to this.setState({spans: spans});
+
   }
   render () {
     // console.log(this.props);
     const {description, urls} = this.props.image;
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         {/* this img is not an HTML tag, this is jsx code from which React will generate HTML */}
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
